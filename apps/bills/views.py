be669a4fc9d321127e8c707f24c5e653a4ba885e
Alex Shvarev <alex@shvarev.com>
2021-08-08 17:01:15 +0300

    v0.11.3 fixed bugs

--- apps/bills/views.py
+++ apps/bills/views.py
@@ -20,59 +20,60 @@ class CreateBillFormView(TemplateView):
     template_name = 'bills/create-bill-form.html'
 
 
-class CreateBillView(View):
-    def post(self, request, *args, **kwargs):
-        try:
-            comment = request.POST.get('comment', '')
-            amount = float(request.POST['amount'])
-            amount = round(amount, 2)
-            site = request.POST['site']
-
-            bill = Bill.objects.create(comment=comment, amount=amount, site=site)
-
-            response = {
-                'id': bill.id,
-                'payUrl': bill.get_url()
-            }
-
-            return HttpResponse(json.dumps(response), content_type='application/json', status=201)
-        except:
-            return HttpResponse('Something went wrong!', status=400)
+# class CreateBillView(View):
+#     def post(self, request, *args, **kwargs):
+#         try:
+#             comment = request.POST.get('comment', '')
+#             amount = float(request.POST['amount'])
+#             amount = round(amount, 2)
+#             site = request.POST['site']
+#
+#             bill = Bill.objects.create(comment=comment, amount=amount, site=site)
+#
+#             response = {
+#                 'id': bill.id,
+#                 'payUrl': bill.get_url()
+#             }
+#
+#             return HttpResponse(json.dumps(response), content_type='application/json', status=201)
+#         except:
+#             return HttpResponse('Something went wrong!', status=400)
 
 
 @api_view(['POST'])
-def SuccessBillView(request):
+def CreateBillView(request):
     try:
-        data = request.POST['bill']
+        comment = request.POST.get('comment', '')
+        amount = float(request.POST['amount'])
+        amount = round(amount, 2)
+        site = request.POST['site']
 
-        len_QIWI_DB_VERSION = len(f'{settings.QIWI_DB_VERSION}_')
-        bill_id = int(data['billId'][len_QIWI_DB_VERSION:])
+        bill = Bill.objects.create(comment=comment, amount=amount, site=site)
 
-        bill = Bill.objects.get(id=bill_id)
-        bill.status = data['status']['value']
-        bill.save()
+        response = {
+            'id': bill.id,
+            'payUrl': bill.get_url()
+        }
 
-        bill.success()
-
-        return Response({'status': 'ok'}, status=202)
+        return Response(response, status=201)
     except:
         return Response({'status': 'something went wrong'}, status=500)
 
 
-# class SuccessBillView(View):
-#     def post(self, request, *args, **kwargs):
-#         try:
-#             data = request.POST['bill']
-#
-#             len_QIWI_DB_VERSION = len(f'{settings.QIWI_DB_VERSION}_')
-#             bill_id = int(data['billId'][len_QIWI_DB_VERSION:])
-#
-#             bill = Bill.objects.get(id=bill_id)
-#             bill.status = data['status']['value']
-#             bill.save()
-#
-#             bill.success()
-#
-#             return HttpResponse('Ok!', status=202)
-#         except:
-#             return HttpResponse('Something went wrong!', status=500)
+class SuccessBillView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = request.POST['bill']
+
+            len_QIWI_DB_VERSION = len(f'{settings.QIWI_DB_VERSION}_')
+            bill_id = int(data['billId'][len_QIWI_DB_VERSION:])
+
+            bill = Bill.objects.get(id=bill_id)
+            bill.status = data['status']['value']
+            bill.save()
+
+            bill.success()
+
+            return HttpResponse('Ok!', status=202)
+        except:
+            return HttpResponse('Something went wrong!', status=500)
